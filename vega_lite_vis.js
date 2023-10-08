d17848213b6ba05cc40da092e3dcc5d539e10cd9
FelixCCN <145447480+FelixCCN@users.noreply.github.com>
2023-10-08 19:42:36 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "map.json";
+var vg_2 = "chart.json";
+vegaEmbed("#map_graph", vg_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#chart_graph", vg_2).then(function(result) {}).catch(console.error);
